b8651d0c7ec6e03c18dff56ccbf514df1ff5237d
Aaron Cohn <aacohn84@gmail.com>
2014-04-28 14:48:10 -0700

    Print and test

--- src/core/Cryptography.java
+++ src/core/Cryptography.java
@@ -15,21 +15,25 @@ public class Cryptography {
 	{
 		//Select two prime numbers p and q.
 		BigInteger p = BigInteger.probablePrime(32, new Random());
+		System.out.println("p: " + p);
 		BigInteger q = BigInteger.probablePrime(32, new Random());
+		System.out.println("q: " + q);
 		
 		//Calculate n;
 		BigInteger n = p.multiply(q);
+		System.out.println("n: " + n);
 		
 		//Calculate phi(n). phiN = (p - 1) * (q - 1)
 		BigInteger phiN = (p.subtract(BigInteger.ONE)).multiply((q.subtract(BigInteger.ONE)));
+		System.out.println("phi(n): " + phiN);
 		
 		//Select relatively prime e.
-		BigInteger e = BigInteger.probablePrime(32, new Random());
-		while(phiN.gcd(e).compareTo(BigInteger.ONE) != 0 || e.compareTo(phiN) > -1)
-			e = BigInteger.probablePrime(32, new Random());
+		BigInteger e = relativePrimeTo(phiN);
+		System.out.println("e: " + e);
 		
 		//Determine d
 		BigInteger d = e.modInverse(phiN);
+		System.out.println("d: " + d);
 		
 		//Create our keys
 		Key pubkey = new Key(e, n);
@@ -38,5 +42,27 @@ public class Cryptography {
 		RSAKeyPair keys = new RSAKeyPair(pubkey, privkey);
 		
 		return keys;
+	}
+
+	private static BigInteger relativePrimeTo(BigInteger phiN) {
+		BigInteger e = BigInteger.probablePrime(32, new Random());
+		while(phiN.gcd(e).compareTo(BigInteger.ONE) != 0 || e.compareTo(phiN) > -1)
+			e = BigInteger.probablePrime(32, new Random());
+		return e;
+	}
+	
+	public static BigInteger encrypt(BigInteger plainText, Key publicKey) {
+		BigInteger e = publicKey.getFirst();
+		BigInteger n = publicKey.getSecond();
+		
+		// C = M^e mod n
+		return plainText.modPow(e, n);
+	}
+	
+	public static BigInteger decrypt(BigInteger cipherText, Key privateKey) {
+		BigInteger d = privateKey.getFirst();
+		BigInteger n = privateKey.getSecond();
+		
+		return cipherText.modPow(d, n);
 	}
 }
--- src/core/RSATest.java
+++ src/core/RSATest.java
@@ -1,5 +1,8 @@
 package core;
 
+import java.math.BigInteger;
+import java.util.Random;
+
 /*
  * A program that allows you to encrypt and decrypt messages with RSA.
  * 
@@ -9,7 +12,15 @@ package core;
 public class RSATest {
 
 	public static void main(String[] args) {
-
+		RSAKeyPair keys = Cryptography.generateRSAKeys();
+		
+		BigInteger plainText = BigInteger.probablePrime(32, new Random());
+		System.out.println("Message: " + plainText);
+		
+		BigInteger cipherText = Cryptography.encrypt(plainText, keys.getPublicKey());
+		System.out.println("Cipher: " + cipherText);
+		
+		BigInteger decodedCipher = Cryptography.decrypt(cipherText, keys.getPrivateKey());
+		System.out.println("Original text: " + decodedCipher);
 	}
-
 }
