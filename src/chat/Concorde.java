2e824919fcc771b1852043cfad23e5744fceeb43
Aaron Cohn <aacohn84@gmail.com>
2014-05-02 23:51:18 -0700

    Implement Concorde's listening and receiving methods

--- src/chat/Concorde.java
+++ src/chat/Concorde.java
@@ -1,31 +1,34 @@
 package chat;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 import crypto.Cryptography;
+import crypto.Key;
 import crypto.RSAKeyPair;
 
 public class Concorde {
 
 	private Bow bow;
 	private ServerSocket serverSocket;
-	private ConnectionListener listener;
+	private ConnectionListener connectionListener;
+	private ArrowReceiver arrowReceiver;
 
 	public Concorde(Bow bow) {
 		this.bow = bow;
 	}
 
 	public void listen() throws IOException {
-		if (serverSocket != null || listener != null) {
+		if (serverSocket != null || connectionListener != null) {
 			throw new RuntimeException("Concorde cannot be told to listen when"
 					+ " he is already listening for new connections.");
 		}
 		serverSocket = new ServerSocket(bow.getPort());
-		listener = new ConnectionListener(serverSocket, bow);
-		listener.start();
+		connectionListener = new ConnectionListener(serverSocket, bow, this);
+		connectionListener.start();
 	}
 
 	public void stopAcceptingNewConnections() throws IOException {
@@ -33,24 +36,39 @@ public class Concorde {
 			throw new RuntimeException("Concorde cannot be told to stop "
 					+ "accepting new connections when he is not listening.");
 		}
-		listener.askedToStopListening = true;
+		connectionListener.askedToStopListening = true;
 		serverSocket.close();
 		serverSocket = null;
-		listener = null;
+		connectionListener = null;
 	}
 
 	public void receiveArrows() {
 		// Grab and decrypt/print incoming messages.
+		if (arrowReceiver != null && arrowReceiver.isAlive()) {
+			throw new RuntimeException("Concorde cannot be told to receive "
+					+ "arrows while he's already receiving arrows.");
+		}
+		arrowReceiver = new ArrowReceiver(bow);
+		arrowReceiver.start();
+	}
+
+	public void stopReceivingArrows() {
+		if (arrowReceiver != null) {
+			arrowReceiver.stopReceivingArrows();
+			arrowReceiver = null;
+		}
 	}
 
 	private static class ConnectionListener extends Thread {
 		private ServerSocket serverSocket;
 		private Bow bow;
-		private boolean askedToStopListening;
+		private Concorde concorde;
+		private boolean askedToStopListening = false;
 
-		public ConnectionListener(ServerSocket serverSocket, Bow bow) {
+		public ConnectionListener(ServerSocket serverSocket, Bow bow, Concorde concorde) {
 			this.serverSocket = serverSocket;
 			this.bow = bow;
+			this.concorde = concorde;
 		}
 
 		@Override
@@ -74,6 +92,8 @@ public class Concorde {
 					outLine.writeObject(rsaKeys.getPublicKey());
 
 					outLine.close();
+					
+					concorde.receiveArrows();
 				} else {
 					// bow may be holding an active connection
 					clientSocket.close();
@@ -86,4 +106,61 @@ public class Concorde {
 		}
 	}
 
+	private static class ArrowReceiver extends Thread {
+
+		private Bow bow;
+		private boolean askedToStop = false;
+
+		public ArrowReceiver(Bow bow) {
+			this.bow = bow;
+		}
+
+		@Override
+		public void run() {
+			if (bow.getChatState() == ChatState.EXCHANGING) {
+				// try to receive a public key from the other party
+				boolean keyReceived = false;
+				while (!keyReceived) {
+					try {
+						keyReceived = receiveKey();
+					} catch (IOException | ClassNotFoundException e) {
+						e.printStackTrace();
+						return;
+					}
+				}
+			}
+			if (bow.getChatState() == ChatState.CONNECTED) {
+				String message;
+				while ((message = bow.receiveArrow()) != null && !askedToStop) {
+					System.out.println(message);
+				}
+			}
+		}
+
+		private boolean receiveKey() throws IOException, ClassNotFoundException {
+			boolean keyReceived = false;
+			Socket socket = bow.getSocket();
+			if (socket == null || socket.isClosed()) {
+				throw new RuntimeException("Can't receive arrows "
+						+ "because there is no active connection.");
+			}
+			ObjectInputStream inLine = new ObjectInputStream(
+					socket.getInputStream());
+			Object object = inLine.readObject();
+			if (object instanceof Key) {
+				bow.setTheirPublicKey((Key) object);
+				keyReceived = true;
+				bow.setChatState(ChatState.CONNECTED);
+			} else {
+				System.out.println("Received something other than "
+						+ "a key while exchanging. Still waiting "
+						+ "for a key.");
+			}
+			return keyReceived;
+		}
+
+		public void stopReceivingArrows() {
+			askedToStop = true;
+		}
+	}
 }
